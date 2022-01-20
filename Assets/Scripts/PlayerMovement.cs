5ceed6a03980afa69c3eddec3926287500fa87ad
sophia <sh01806@srcs.org>
2022-01-20 15:38:33 -0800

    camera scene completed

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -2,17 +2,50 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class NewBehaviourScript : MonoBehaviour
+public class PlayerMovement : MonoBehaviour
 {
+    private Animator animator;
+    private Rigidbody rb;
+    private Vector3 moveDirection;
+    private Quaternion rotation;
+    private bool isWalking;
+
+    [SerializeField] private float turnSpeed = 20f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody>();
+        rotation = Quaternion.identity;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void FixedUpdate()
     {
-        
+        // get user input
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Verticle");
+
+
+
+        //user user input to set direction player will move
+        moveDirection.Set(horizontal, 0f, vertical);
+        moveDirection.Normalize();
+        //set animator to walking or ide depending on user input
+        isWalking = !(Mathf.Approximately(horizontal, 0f) && Mathf.Approximately(vertical, 0f));
+        animator.SetBool("isWalking", isWalking);
+        //assign rotation towards move direction
+        Vector3 desiredDirection = Vector3.RotateTowards(transform.forward, moveDirection,
+                turnSpeed * Time.deltaTime, 0f);
+        rotation = Quaternion.LookRotation(desiredDirection);
     }
+
+
+    // Animator event
+    private void OnAnimatorMove()
+    {
+        rb.MovePosition(rb.position + moveDirection * animator.deltaPosition.magnitude);
+        rb.MoveRotation(rotation);
+    }
+
 }
