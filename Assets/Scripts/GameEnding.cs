77dc62f431c318ce9b9e67e7f1b8bfa36f3af8ee
sophia <sh01806@srcs.org>
2022-01-31 15:47:16 -0800

    ending the game

---
+++ Assets/Scripts/GameEnding.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameEnding : MonoBehaviour
+{
+    public float fadeDuration = 1f;
+    public GameEndingObject player;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == player )
+        {
+
+        }
+    }
+}
+
+
+
+
