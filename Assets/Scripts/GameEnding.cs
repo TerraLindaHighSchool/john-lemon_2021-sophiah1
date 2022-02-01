baded690975308ee421221cd1ac6f05133bc8b9c
sophia <sh01806@srcs.org>
2022-02-01 15:32:00 -0800

    ending the game complete

--- Assets/Scripts/GameEnding.cs
+++ Assets/Scripts/GameEnding.cs
@@ -5,17 +5,39 @@ using UnityEngine;
 public class GameEnding : MonoBehaviour
 {
     public float fadeDuration = 1f;
-    public GameEndingObject player;
+    public float displayImageDuration = 1f;
+    public GameObject player;
+    public CanvasGroup exitBackgroundImageCanvasGroup;
 
-    private void OnTriggerEnter(Collider other)
+    bool m_IsPlayerAtExit;
+    float m_Timer;
+
+    void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player )
+        if (other.gameObject == player)
         {
+            m_IsPlayerAtExit = true;
+        }
+    }
 
+    void Update()
+    {
+        if (m_IsPlayerAtExit)
+        {
+            EndLevel();
+        }
+    }
+
+    void EndLevel()
+    {
+        m_Timer += Time.deltaTime;
+
+        exitBackgroundImageCanvasGroup.alpha = m_Timer / fadeDuration;
+
+        if (m_Timer > fadeDuration + displayImageDuration)
+        {
+            Application.Quit();
         }
     }
 }
-
-
-
 
